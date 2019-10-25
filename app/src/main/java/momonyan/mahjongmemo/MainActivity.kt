0180ddfa22adae15b00c3835958b6a4e525c6bb8
“BlueWind_Mac” <zero18030@gmail.com>
2019-10-25 18:39:01 +0900

    [add,update]名前登録ダイアログの実装。 一応メイン画面に表示するまで。

--- app/src/main/java/momonyan/mahjongmemo/MainActivity.kt
+++ app/src/main/java/momonyan/mahjongmemo/MainActivity.kt
@@ -1,12 +1,31 @@
 package momonyan.mahjongmemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.name_setting_dialog.view.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val dialog = AlertDialog.Builder(this)
+            val dialog_view = layoutInflater.inflate(R.layout.name_setting_dialog, null)
+
+
+            dialog.setView(dialog_view)
+                .setPositiveButton("OK") { _, _ ->
+                    nameTextView.text = dialog_view.input1.text.toString()
+                    nameTextView2.text = dialog_view.input2.text.toString()
+                    nameTextView3.text = dialog_view.input3.text.toString()
+                    nameTextView4.text = dialog_view.input4.text.toString()
+                }
+                .show()
+
+        }
     }
 }
