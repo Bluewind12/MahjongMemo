3e30bcdd79bd3ceee946c4d1a5177d799ad84f0d
“BlueWind_Mac” <zero18030@gmail.com>
2019-11-12 01:19:13 +0900

    [update]大まかな流れの完成 点数入力 Text表示 局数変更

--- app/src/main/java/momonyan/mahjongmemo/MainActivity.kt
+++ app/src/main/java/momonyan/mahjongmemo/MainActivity.kt
@@ -1,23 +1,102 @@
 package momonyan.mahjongmemo
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.GridLayout
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.data_add_dialog.view.*
 import kotlinx.android.synthetic.main.name_setting_dialog.view.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var names = arrayListOf("プレイヤー1", "プレイヤー2", "プレイヤー3", "プレイヤー4")
+    private var points = arrayListOf<Int>()
+    private var stageCount = 1
+    private var stageCountDiff = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+
         dataAddFloatActionButton.setOnClickListener {
+            val addDialog = AlertDialog.Builder(this)
+            val dialogView = layoutInflater.inflate(R.layout.data_add_dialog, null)
+            dialogView.dataAddNameTextView.text = names[0]
+            dialogView.dataAddNameTextView2.text = names[1]
+            dialogView.dataAddNameTextView3.text = names[2]
+            dialogView.dataAddNameTextView4.text = names[3]
 
+            val dataInputTextViews = arrayListOf(
+                dialogView.dataAddTextInput,
+                dialogView.dataAddTextInput2,
+                dialogView.dataAddTextInput3,
+                dialogView.dataAddTextInput4
+            )
 
+            addDialog.setView(dialogView)
+                .setPositiveButton("追加") { _, _ ->
+                    points = arrayListOf()
+                    dataInputTextViews.forEach {
+                        if (it.text.toString() == "") {
+                            points.add(0)
+                        } else {
+                            points.add(it.text.toString().toInt())
+                        }
+                    }
+
+                    //局数の表示
+                    if (stageCount != stageCountDiff) {
+                        val stageTextView = TextView(applicationContext)
+                        val stageString = when ((stageCount % 16) / 4) {
+                            0 -> "東"
+                            1 -> "南"
+                            2 -> "西"
+                            3 -> "北"
+                            else -> "エラー"
+                        }
+                        stageTextView.text = stageString + (stageCount % 4 + 1) + "局"
+
+                        val gridParams =
+                            GridLayout.LayoutParams(
+                                GridLayout.spec(stageCount),
+                                GridLayout.spec(0)
+                            )
+
+                        stageTextView.layoutParams = gridParams
+                        mainGridLayout.addView(stageTextView)
+
+                        //直りん
+                        stageCountDiff = stageCount
+                    }
+
+                    //点数表示
+                    for (i in 0 until points.size) {
+                        val textView = TextView(applicationContext)
+                        textView.text = points[i].toString()
+                        if (points[i] < 0) {
+                            textView.setTextColor(Color.RED)
+                        }
+                        val gridParams =
+                            GridLayout.LayoutParams(
+                                GridLayout.spec(stageCount),
+                                GridLayout.spec(i + 1)
+                            )
+                        textView.layoutParams = gridParams
+                        mainGridLayout.addView(textView)
+                    }
+
+                    //流れたかどうかの確認
+                    if (dialogView.dataAddFlowToggleButton.isChecked) {
+                        stageCount++
+                    }
+                }
 
         }
 
@@ -37,6 +116,15 @@ class MainActivity : AppCompatActivity() {
 
                 dialog.setView(dialogView)
                     .setPositiveButton("OK") { _, _ ->
+                        //データ入れ
+                        names = arrayListOf(
+                            dialogView.input1.text.toString(),
+                            dialogView.input2.text.toString(),
+                            dialogView.input3.text.toString(),
+                            dialogView.input4.text.toString()
+                        )
+
+                        //表示変更
                         nameTextView.text = dialogView.input1.text.toString()
                         nameTextView2.text = dialogView.input2.text.toString()
                         nameTextView3.text = dialogView.input3.text.toString()
